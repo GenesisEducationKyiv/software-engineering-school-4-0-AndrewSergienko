48e1e3a82980bb78fd4ea4cbbd2350cc74f63cf2
andrew <sergienkoandrew9922@gmail.com>
2024-06-14 23:20:33 +0300

    refactor: remove unnecessary functions and comments

--- internal/api/currencyRate.go
+++ internal/api/currencyRate.go
@@ -1,39 +1,9 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"go_service/internal/common"
-	"net/http"
 )
-
-func GetCurrencyHandler(cr common.CurrencyReader) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			_, err := fmt.Fprintf(w, "Method Not Allowed")
-			if err != nil {
-				return
-			}
-		}
-
-		rate, err := cr.GetUSDCurrencyRate()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		response := map[string]interface{}{
-			"rate": rate,
-		}
-
-		err = json.NewEncoder(w).Encode(response)
-		if err != nil {
-			return
-		}
-	}
-}
 
 type CurrencyHandlers struct {
 	currencyGateway common.CurrencyReader
--- internal/api/subscribers.go
+++ internal/api/subscribers.go
@@ -12,43 +12,6 @@ type SubscriberGateway interface {
 	common.SubscriberReader
 }
 
-//func GetSubscribersHandler(sg SubscriberGateway) http.HandlerFunc {
-//	return func(w http.ResponseWriter, r *http.Request) {
-//		if r.Method != http.MethodPost {
-//			w.WriteHeader(http.StatusMethodNotAllowed)
-//			return
-//		}
-//
-//		var requestData struct {
-//			Email string `json:"email"`
-//		}
-//
-//		err := json.NewDecoder(r.Body).Decode(&requestData)
-//		if err != nil {
-//			w.WriteHeader(http.StatusBadRequest)
-//			return
-//		}
-//
-//		if requestData.Email == "" {
-//			w.WriteHeader(http.StatusBadRequest)
-//			return
-//		}
-//
-//		subscriber := sg.GetByEmail(requestData.Email)
-//		if subscriber != nil {
-//			w.WriteHeader(http.StatusConflict)
-//			return
-//		}
-//
-//		err = sg.Create(requestData.Email)
-//		if err != nil {
-//			w.WriteHeader(http.StatusAccepted)
-//			return
-//		}
-//		w.WriteHeader(http.StatusOK)
-//	}
-//}
-
 type SubscribersHandlers struct {
 	subscriberGateway SubscriberGateway
 }
